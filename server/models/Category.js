2d1e7b6d2fe89fbd047fca94d31fee0834e269d9
Andrea Ramirez <andrea.anza3@gmail.com>
2023-05-22 19:43:21 -0500

    Finished models

--- server/models/Category.js
+++
@@ -1,5 +0,0 @@
-/*Category Model:
-- Represents the categories or types of products available.
-- Fields may include name, description, parent category reference, etc.
-- Allows for organizing products into different categories and subcategories.
-*/
