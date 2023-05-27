951f1d292afaff85cd35706a970876853acd4c96
fabri-tech <fabriziotrevino@gmail.com>
2023-05-27 14:36:31 -0600

    added components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,39 @@
-import Hero from "./components/hero.jsx"
+import Hero from "./components/hero.jsx";
+import Login from "./pages/login.jsx";
+import NewUser from "./pages/newUser.jsx";
+import Review from "./pages/review.jsx";
+import ProSearch from "./components/proSearch.jsx";
+import JobCategory from "./pages/jobCategory.jsx";
+import Pricing from "./components/pricing.jsx";
+import Footer from "./components/footer.jsx";
+import ErrorPage from "./pages/errorPage.jsx";
+import Team from "./components/team.jsx";
+import Testimonials from "./components/testimonials.jsx";
+import Profile from "./pages/profile.jsx";
+
+// import ErrorPage from "./components/footer.jsx";
+
+
 import 'tailwindcss/tailwind.css';
 
 function App() {
-
   return (
-  <>
-  <Hero/>
-  </>
+    <>
+      <Hero />
+      <Login />
+      <NewUser />       
+      <Profile />
+      <Review />
+      <ProSearch />
+      <JobCategory />
+      <Pricing />      
+      <Testimonials />   
+      <Team />
+      <Footer />
+      {/* <ErrorPage /> */}
+
+          </>
   );
 }
 
-export default App
+export default App;
---
+++ client/src/pages/jobCategory.jsx
@@ -0,0 +1,78 @@
+const people = [
+  {
+    name: 'Carpintería',
+    imageUrl: 'https://images.unsplash.com/photo-1626081062126-d3b192c1fcb0?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=876&q=80',
+  },
+  {
+    name: 'Catering',
+    imageUrl: 'https://images.unsplash.com/photo-1623475173140-ad2f0369ca92?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1024&q=80',
+  },
+  {
+    name: 'Contabilidad',
+    imageUrl: 'https://images.unsplash.com/photo-1563212034-a3c52118cce2?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+  },
+  {
+    name: 'Diseño Gráfico',
+    imageUrl: 'https://images.unsplash.com/photo-1626785774573-4b799315345d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1171&q=80',
+  },
+  {
+    name: 'Electricista',
+    imageUrl: 'https://images.unsplash.com/photo-1555963966-b7ae5404b6ed?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+  },
+  {
+    name: 'Florista',
+    imageUrl: 'https://images.unsplash.com/photo-1527609635833-38e4a5f7c941?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80',
+  },
+  {
+    name: 'Fotografía',
+    imageUrl: 'https://images.unsplash.com/photo-1516035069371-29a1b244cc32?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=764&q=80',
+  },
+  {
+    name: 'Jardinería',
+    imageUrl: 'https://images.unsplash.com/photo-1416879595882-3373a0480b5b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80',
+  },
+  {
+    name: 'Limpieza',
+    imageUrl: 'https://images.unsplash.com/photo-1528740561666-dc2479dc08ab?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80',
+  },
+  {
+    name: 'Pintura',
+    imageUrl: 'https://images.unsplash.com/photo-1621685682093-3b8016dcb57d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=873&q=80',
+  },
+  {
+    name: 'Plomería',
+    imageUrl: 'https://images.unsplash.com/photo-1543674892-7d64d45df18b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=730&q=80',
+  },
+  {
+    name: 'Programación',
+    imageUrl: 'https://images.unsplash.com/photo-1504639725590-34d0984388bd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1074&q=80',
+  },
+  // Otras personas...
+];
+
+export default function Example() {
+  return (
+    <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto max-w-2xl lg:mx-0">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Encontrar Pro</h2>
+          <p className="mt-6 text-lg leading-8 text-gray-600">
+            Descubre soluciones rápidas y eficientes con expertos altamente calificados en nuestra plataforma. ¡Inicia tu búsqueda ahora y transforma tu situación!
+          </p>
+        </div>
+        <ul
+          role="list"
+          className="mx-auto mt-20 grid max-w-2xl grid-cols-2 gap-x-8 gap-y-16 text-center sm:grid-cols-3 md:grid-cols-4 lg:mx-0 lg:max-w-none lg:grid-cols-5 xl:grid-cols-6"
+        >
+          {people.map((person) => (
+            <li key={person.name}>
+              <img className="mx-auto h-24 w-24 rounded-full" src={person.imageUrl} alt="" />
+              <h3 className="mt-6 text-base font-semibold leading-7 tracking-tight text-gray-900">{person.name}</h3>
+              <p className="text-sm leading-6 text-gray-600">{person.role}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
