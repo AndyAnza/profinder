96da6c827d5564d0df59fae055c9dc1b5f3dd3ad
Andrea Ramirez <andrea.anza3@gmail.com>
2023-05-28 14:49:38 -0500

    Added back end functionality to createUser.jsx - still need to fix location, but its working now

--- client/src/pages/customerUser.jsx
+++ client/src/pages/customerUser.jsx
@@ -1,12 +1,46 @@
-import { useState } from "react";
-import { Switch } from "@headlessui/react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { ADD_USER } from "../utils/mutations";
+import Auth from "../utils/auth";
 
-function classNames(...classes) {
-  return classes.filter(Boolean).join(" ");
-}
+const Signup = () => {
+  const [formState, setFormState] = useState({
+    username: "",
+    name: "",
+    lastname: "",
+    email: "",
+    password: "",
+    phone: "",
+    location: "",
+    profilePicture: "",
+  });
 
-export default function SignIn() {
-  const [agreed, setAgreed] = useState(false);
+  const [addUser, { data, error }] = useMutation(ADD_USER);
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await addUser({
+        variables: { ...formState },
+      });
+
+      Auth.login(data.addUser.token);
+    } catch (e) {
+      console.error(e);
+    }
+  };
 
   return (
     <div className="isolate bg-indigo-600 px-6 py-24 sm:py-28 lg:px-8">
@@ -23,193 +57,192 @@ export default function SignIn() {
         />
       </div>
       <div className="mx-auto mt-8 max-w-xl sm:mt-6 bg-white border border-gray-300 rounded-lg px-8 py-6">
-        <form
-          action="#"
-          method="POST"
-          className="mx-auto mt-16 max-w-xl sm:mt-6"
-        >
-          <div className="flex justify-center col-span-2 mb-8">
-            <label
-              htmlFor="registrate"
-              className="block text-2xl font-bold text-gray-900 mb-2"
-            >
-              Crea una cuenta
-            </label>
-          </div>
-          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
-            <div>
+        {data ? (
+          <p>
+            Success! You may now head <Link to="/">back to the homepage.</Link>
+          </p>
+        ) : (
+          <form className="mx-auto mt-16 max-w-xl sm:mt-6">
+            <div className="flex justify-center col-span-2 mb-8">
               <label
-                htmlFor="first-name"
-                className="block text-sm font-semibold leading-6 text-gray-900"
+                htmlFor="registrate"
+                className="block text-2xl font-bold text-gray-900 mb-2"
               >
-                Nombre
+                Crea una cuenta
               </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="first-name"
-                  id="first-name"
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
             </div>
-            <div>
-              <label
-                htmlFor="last-name"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Apellido
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="last-name"
-                  id="last-name"
-                  autoComplete="family-name"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Nombre
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="name"
+                    id="first-name"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={formState.name}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="company"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Nombre de Usuario
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="text"
-                  name="company"
-                  id="company"
-                  autoComplete="organization"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+              <div>
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Apellido
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="lastname"
+                    id="last-name"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={formState.lastname}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="email"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Email
-              </label>
-              <div className="mt-2.5">
-                <input
-                  type="email"
-                  name="email"
-                  id="email"
-                  autoComplete="email"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="username"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Nombre de Usuario
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    autoComplete="username"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={formState.username}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
-            </div>
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="phone-number"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Telefono
-              </label>
-              <div className="relative mt-2.5">
-                <div className="absolute inset-y-0 left-0 flex items-center"></div>
-                <input
-                  type="tel"
-                  name="phone-number"
-                  id="phone-number"
-                  autoComplete="tel"
-                  className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    type="email"
+                    name="email"
+                    id="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={formState.email}
+                    onChange={handleChange}
+                  />
+                </div>
               </div>
-            </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Telefono
+                </label>
+                <div className="relative mt-2.5">
+                  <div className="absolute inset-y-0 left-0 flex items-center"></div>
+                  <input
+                    type="tel"
+                    name="phone"
+                    id="phone-number"
+                    autoComplete="tel"
+                    className="block w-full rounded-md border-0 px-3.5 py-2 pl-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    value={formState.phone}
+                    onChange={handleChange}
+                  />
+                </div>
+              </div>
 
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="location"
-                className="block text-sm font-semibold leading-6 text-gray-900"
-              >
-                Ubicación
-              </label>
-              <div className="relative">
-                <select className="block appearance-none w-full py-2 px-4 pr-8 rounded-md border border-gray-300 bg-white shadow-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
-                  <option>CDMX</option>
-                  <option>Monterrey</option>
-                </select>
-                <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
-                  <svg
-                    className="h-4 w-4 text-gray-500"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="location"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Ubicación
+                </label>
+                <div className="relative">
+                  <select
+                    className="block appearance-none w-full py-2 px-4 pr-8 rounded-md border border-gray-300 bg-white shadow-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                    name="location"
+                    value={formState.location}
+                    onChange={handleChange}
+                    // onChange={(event) =>
+                    //   setFormState.location(event.target.value)
+                    // }
                   >
-                    <path
-                      fillRule="evenodd"
-                      d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 4.414V16a1 1 0 01-2 0V4.414L5.707 7.707a1 1 0 01-1.414 0z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
+                    <option>CDMX</option>
+                    <option>Monterrey</option>
+                  </select>
+                  <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                    <svg
+                      className="h-4 w-4 text-gray-500"
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 20 20"
+                      fill="currentColor"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 4.414V16a1 1 0 01-2 0V4.414L5.707 7.707a1 1 0 01-1.414 0z"
+                        clipRule="evenodd"
+                      />
+                    </svg>
+                  </div>
+                </div>
+              </div>
+
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="password"
+                  className="block text-sm font-semibold leading-6 text-gray-900"
+                >
+                  Contraseña
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    className="form-input block w-full px-4 py-2 rounded-md border border-gray-300 bg-white shadow-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                    placeholder="******"
+                    name="password"
+                    type="password"
+                    value={formState.password}
+                    onChange={handleChange}
+                  />
                 </div>
               </div>
             </div>
-
-            <div className="sm:col-span-2">
-              <label
-                htmlFor="password"
-                className="block text-sm font-semibold leading-6 text-gray-900"
+            <div className="flex justify-center sm:justify-end mt-8">
+              <button
+                type="submit"
+                className="inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+                onClick={handleFormSubmit}
               >
-                Contraseña
-              </label>
-              <div className="mt-2.5">
-                <input
-                  className="form-input block w-full px-4 py-2 rounded-md border border-gray-300 bg-white shadow-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  // value={formState.password}
-                  // onChange={handleChange}
-                />
-              </div>
+                Registrarse
+              </button>
             </div>
-            <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
-              <div className="flex h-6 items-center">
-                <Switch
-                  checked={agreed}
-                  onChange={setAgreed}
-                  className={classNames(
-                    agreed ? "bg-indigo-600" : "bg-gray-200",
-                    "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                  )}
-                >
-                  <span className="sr-only">Agree to policies</span>
-                  <span
-                    aria-hidden="true"
-                    className={classNames(
-                      agreed ? "translate-x-3.5" : "translate-x-0",
-                      "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
-                    )}
-                  />
-                </Switch>
-              </div>
-              <Switch.Label className="text-sm leading-6 text-gray-600">
-                By selecting this, you agree to our{" "}
-                <a href="#" className="font-semibold text-indigo-600">
-                  privacy&nbsp;policy
-                </a>
-                .
-              </Switch.Label>
-            </Switch.Group>
-          </div>
-          <div className="mt-10">
-            <button
-              type="submit"
-              className="block w-full rounded-md bg-violet-900 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-violet-950 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-            >
-              Enviar
-            </button>
-          </div>
-        </form>
+          </form>
+        )}
+
+        {error && (
+          <div className="my-3 p-3 bg-danger text-white">{error.message}</div>
+        )}
       </div>
     </div>
   );
-}
+};
+
+export default Signup;
