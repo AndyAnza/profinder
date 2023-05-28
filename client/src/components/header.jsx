46dd3e74bef8a11f5a1d9273bce12b0d192418a6
Andrea Ramirez <andrea.anza3@gmail.com>
2023-05-28 04:59:16 -0500

    Changed sign-in form - still in WIP. Added home route to logo in the header

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -16,13 +16,13 @@ export default function Header() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className="absolute inset-x-0 top-0 z-50">
+    <header className="absolute inset-x-0 top-0 z-50 bg-white">
       <nav
         className="mx-auto flex max-w-7xl items-center justify-between lg:px-8"
         aria-label="Global"
       >
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">Tu Compañía</span>
             <img className="h-28 w-auto" src="/proFinder_Logo.png" alt="" />
           </a>
--- client/src/pages/customerUser.jsx
+++ client/src/pages/customerUser.jsx
@@ -1,184 +1,215 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid';
+import { useState } from "react";
+import { Switch } from "@headlessui/react";
 
-export default function Ejemplo() {
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default function SignIn() {
+  const [agreed, setAgreed] = useState(false);
+
   return (
-    <form>
-      <div className="grid grid-cols-1 gap-x-8 gap-y-10 border-b border-gray-900/10 pb-12 md:grid-cols-3 ml-4">
-        <div>
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Información personal</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">
-            Utiliza una dirección permanente donde puedas recibir correo.
-          </p>
-        </div>
-
-        <div className="grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6 md:col-span-2">
-          <div className="sm:col-span-3">
-            <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
-              Nombre
-            </label>
-            <div className="mt-2">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div className="sm:col-span-3">
-            <label htmlFor="last-name" className="block text-sm font-medium leading-6 text-gray-900">
-              Apellido
-            </label>
-            <div className="mt-2">
-              <input
-                type="text"
-                name="last-name"
-                id="last-name"
-                autoComplete="family-name"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div className="col-span-full">
-            <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
-              Foto
-            </label>
-            <div className="mt-2 flex items-center gap-x-3">
-              <UserCircleIcon className="h-12 w-12 text-gray-300" aria-hidden="true" />
-              <button
-                type="button"
-                className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
-              >
-                Cambiar
-              </button>
-            </div>
-          </div>
-
-          <div className="sm:col-span-4">
-            <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-              Dirección de correo electrónico
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div className="sm:col-span-4">
-            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900">
-              Nombre de Usuario
-            </label>
-            <div className="mt-2">
-              <input
-                id="username"
-                name="username"
-                type="text"
-                autoComplete="username"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div className="sm:col-span-3">
-            <label htmlFor="country" className="block text-sm font-medium leading-6 text-gray-900">
-              País
-            </label>
-            <div className="mt-2">
-              <select
-                id="country"
-                name="country"
-                autoComplete="country-name"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-              >
-                <option>Estados Unidos</option>
-                <option>Canadá</option>
-                <option>México</option>
-              </select>
-            </div>
-          </div>
-
-          <div className="sm:col-span-2 sm:col-start-1">
-            <label htmlFor="city" className="block text-sm font-medium leading-6 text-gray-900">
-              Ciudad
-            </label>
-            <div className="mt-2">
-              <input
-                type="text"
-                name="city"
-                id="city"
-                autoComplete="address-level2"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div className="col-span-full">
-            <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-gray-900">
-              Dirección
-            </label>
-            <div className="mt-2">
-              <input
-                type="text"
-                name="street-address"
-                id="street-address"
-                autoComplete="street-address"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div className="sm:col-span-2">
-            <label htmlFor="region" className="block text-sm font-medium leading-6 text-gray-900">
-              Estado / Provincia
-            </label>
-            <div className="mt-2">
-              <input
-                type="text"
-                name="region"
-                id="region"
-                autoComplete="address-level1"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div className="sm:col-span-2">
-            <label htmlFor="postal-code" className="block text-sm font-medium leading-6 text-gray-900">
-              Código postal
-            </label>
-            <div className="mt-2">
-              <input
-                type="text"
-                name="postal-code"
-                id="postal-code"
-                autoComplete="postal-code"
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-        </div>
+    <div className="isolate bg-indigo-600 px-6 py-24 sm:py-28 lg:px-8">
+      <div
+        className="absolute inset-x-0 top-[-10rem] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[-20rem]"
+        aria-hidden="true"
+      >
+        <div
+          className="relative left-1/2 -z-10 aspect-[1155/678] w-[36.125rem] max-w-none -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-40rem)] sm:w-[72.1875rem]"
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+        />
       </div>
+      <div className="mx-auto mt-8 max-w-xl sm:mt-6 bg-white border border-gray-300 rounded-lg px-8 py-6">
+        <form
+          action="#"
+          method="POST"
+          className="mx-auto mt-16 max-w-xl sm:mt-6"
+        >
+          <div className="flex justify-center col-span-2 mb-8">
+            <label
+              htmlFor="registrate"
+              className="block text-2xl font-bold text-gray-900 mb-2"
+            >
+              Crea una cuenta
+            </label>
+          </div>
+          <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+            <div>
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Nombre
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div>
+              <label
+                htmlFor="last-name"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Apellido
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="last-name"
+                  id="last-name"
+                  autoComplete="family-name"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="company"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Nombre de Usuario
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="text"
+                  name="company"
+                  id="company"
+                  autoComplete="organization"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="email"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Email
+              </label>
+              <div className="mt-2.5">
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="phone-number"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Telefono
+              </label>
+              <div className="relative mt-2.5">
+                <div className="absolute inset-y-0 left-0 flex items-center"></div>
+                <input
+                  type="tel"
+                  name="phone-number"
+                  id="phone-number"
+                  autoComplete="tel"
+                  className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
-      <div className="flex items-center justify-end gap-x-6">
-  <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-    Cancelar
-  </button>
-  <button
-    type="submit"
-    className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-  >
-    Guardar
-  </button>
-</div>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="location"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Ubicación
+              </label>
+              <div className="relative">
+                <select className="block appearance-none w-full py-2 px-4 pr-8 rounded-md border border-gray-300 bg-white shadow-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500">
+                  <option>CDMX</option>
+                  <option>Monterrey</option>
+                </select>
+                <div className="absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                  <svg
+                    className="h-4 w-4 text-gray-500"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                    fill="currentColor"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M5.293 7.707a1 1 0 010-1.414l4-4a1 1 0 011.414 0l4 4a1 1 0 01-1.414 1.414L11 4.414V16a1 1 0 01-2 0V4.414L5.707 7.707a1 1 0 01-1.414 0z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                </div>
+              </div>
+            </div>
 
-    </form>
+            <div className="sm:col-span-2">
+              <label
+                htmlFor="password"
+                className="block text-sm font-semibold leading-6 text-gray-900"
+              >
+                Contraseña
+              </label>
+              <div className="mt-2.5">
+                <input
+                  className="form-input block w-full px-4 py-2 rounded-md border border-gray-300 bg-white shadow-sm text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500"
+                  placeholder="******"
+                  name="password"
+                  type="password"
+                  // value={formState.password}
+                  // onChange={handleChange}
+                />
+              </div>
+            </div>
+            <Switch.Group as="div" className="flex gap-x-4 sm:col-span-2">
+              <div className="flex h-6 items-center">
+                <Switch
+                  checked={agreed}
+                  onChange={setAgreed}
+                  className={classNames(
+                    agreed ? "bg-indigo-600" : "bg-gray-200",
+                    "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  )}
+                >
+                  <span className="sr-only">Agree to policies</span>
+                  <span
+                    aria-hidden="true"
+                    className={classNames(
+                      agreed ? "translate-x-3.5" : "translate-x-0",
+                      "h-4 w-4 transform rounded-full bg-white shadow-sm ring-1 ring-gray-900/5 transition duration-200 ease-in-out"
+                    )}
+                  />
+                </Switch>
+              </div>
+              <Switch.Label className="text-sm leading-6 text-gray-600">
+                By selecting this, you agree to our{" "}
+                <a href="#" className="font-semibold text-indigo-600">
+                  privacy&nbsp;policy
+                </a>
+                .
+              </Switch.Label>
+            </Switch.Group>
+          </div>
+          <div className="mt-10">
+            <button
+              type="submit"
+              className="block w-full rounded-md bg-violet-900 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-violet-950 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            >
+              Enviar
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
   );
 }
